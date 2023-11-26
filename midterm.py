97e625e42c5cf29f7cfa3a85290458706be6d153
SimaHabbal <simamedrarhabbal@gmai.com>
2023-11-26 16:38:45 +0200

    menu

---
+++ midterm.py
@@ -0,0 +1,12 @@
+def menu():
+  print("Welcome to Sima's midterm answer.")
+  print("Please select one of the following options from the menu:")
+  print("1. Open Tab")
+  print("2. Close Tab")
+  print("3. Switch Tab")
+  print("4. Display All Tabs")
+  print("5. Open Nested Tabs")
+  print("6. Sort All Tabs")
+  print("7. Save Tabs")
+  print("8. Import Tabs")
+  print("9. Exit")
